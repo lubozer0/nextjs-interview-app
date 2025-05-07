7093ad9b6f27e346920a14b8254662394b2d87b6
lubozer0 <luvoslav.iv.stefanov@gmail.com>
2025-05-07 13:35:21 +0300

    Add interview creation and update logic

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,17 +1,28 @@
-import { getInterviewById } from "@/lib/actions/general.action";
-import { redirect } from "next/navigation";
 import Image from "next/image";
-import { getRandomInterviewCover } from "@/lib/utils";
-import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { redirect } from "next/navigation";
+
 import Agent from "@/components/Agent";
+import { getRandomInterviewCover } from "@/lib/utils";
+
+import {
+  getFeedbackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
 import { getCurrentUser } from "@/lib/actions/auth.action";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
 
-const Page = async ({ params }: RouteParams) => {
+const InterviewDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
-  const user = await getCurrentUser();
-  const interview = await getInterviewById(id);
 
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewById(id);
   if (!interview) redirect("/");
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
 
   return (
     <>
@@ -31,19 +42,21 @@ const Page = async ({ params }: RouteParams) => {
           <DisplayTechIcons techStack={interview.techstack} />
         </div>
 
-        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">
+        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit">
           {interview.type}
         </p>
       </div>
 
       <Agent
-        userName={user?.name}
-        type={user?.id}
+        userName={user?.name!}
+        userId={user?.id}
         interviewId={id}
         type="interview"
         questions={interview.questions}
+        feedbackId={feedback?.id}
       />
     </>
   );
 };
-export default Page;
+
+export default InterviewDetails;
